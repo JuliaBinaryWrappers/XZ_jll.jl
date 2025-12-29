7a5bd81b8d2e84e0a603699d6618b467a0cff87a
jlbuild <juliabuildbot@gmail.com>
2025-12-29 19:00:21 +0000

    XZ_jll build 5.8.2+0

--- src/XZ_jll.jl
+++ src/XZ_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("XZ")
-JLLWrappers.@generate_main_file("XZ", UUID("ffd25f8a-64ca-5728-b0f7-c24cf3aae800"))
+JLLWrappers.@generate_main_file("XZ", Base.UUID("ffd25f8a-64ca-5728-b0f7-c24cf3aae800"))
 end  # module XZ_jll
